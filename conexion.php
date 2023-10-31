2a9a43f2df65274999cc72da214dec32de4ec26c
eduperez90 <eduardo.percam@gmail.com>
2023-10-31 10:46:25 +0100

    .

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+$mysql_server = "localhost";
+$mysql_login = "edu_php";
+$mysql_pass = "Bender,01";
+$base = "edu_proyecto";
+
+function conectar(){
+    global $mysql_server, $mysql_login, $mysql_pass, $base;
+    $c = mysqli_connect($mysql_server, $mysql_login, $mysql_pass) or die ("Imposible conectar");
+    mysqli_select_db($c, $base);
+}
+?>
